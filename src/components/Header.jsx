b34952593da7d63b88a8450d636dcb77b6a5e209
AleksandraLos <oluchna@hotmail.com>
2022-08-29 16:56:08 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+const Header = () => {
+    return ( <h1>Header kommer här</h1> );
+}
+ 
+export default Header;
---
+++ src/components/Main.jsx
@@ -0,0 +1,5 @@
+const Main = () => {
+    return (  <h1>Main kommer här</h1>);
+}
+ 
+export default Main;
