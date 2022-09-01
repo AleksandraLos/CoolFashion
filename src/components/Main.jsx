98c6de2a2ad5147726ca7307d61f3436b0a6b1ed
Michiel <47319476+mvdgragt@users.noreply.github.com>
2022-09-01 08:58:51 +0200

    Update Main.jsx
    First Main Upload, 4 cards

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,38 @@
 const Main = () => {
-    return (  <h1>Main kommer här</h1>);
+    return (   <>
+      <div className="App">
+        <h1>Categories</h1>
+      </div>
+      <div className="square-container">
+        <div class="square">
+          <img
+            src="https://images.unsplash.com/photo-1644910377725-1341da4d470a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60"
+            alt=""
+          />
+        </div>
+
+        <div class="square">
+          <img
+            src="https://images.unsplash.com/photo-1508216310976-c518daae0cdc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8c3RyZWV0JTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&w=1000&q=80"
+            alt=""
+          />
+        </div>
+
+        <div class="square">
+          <img
+            src="https://images.unsplash.com/photo-1574427797991-b086946fa9e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+            alt=""
+          />
+        </div>
+
+        <div class="square">
+          <img
+            src="https://i.pinimg.com/564x/24/ed/a3/24eda3a536f165eabd9716edcb935799.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+    </>);
 }
  
 export default Main;
