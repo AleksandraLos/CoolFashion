047db2e782d6bf65c07d8203bc4528e7576743cb
AleksandraLos <oluchna@hotmail.com>
2022-09-02 09:48:29 +0200

    fix css bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -276,9 +276,6 @@ export default function Header() {
                 </>
               )}
             </Popover>
-            <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
-              Sneakers
-            </a>
           </Popover.Group>
           <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
             <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
