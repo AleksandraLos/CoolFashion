7e8ae41f9be284cbbc8b88c1842b8e6cefc39a17
AleksandraLos <oluchna@hotmail.com>
2022-09-02 09:31:24 +0200

    add main to app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Footer from './components/Footer';
 import InstagramCarousel from './components/InstagramCarousel';
 import MainHero from './components/MainHero';
 import Header from'./components/Header';
+import Main from './components/Main'
 
 function App() {
   return (
     <div className='App'>
       <Header/>
       <MainHero />
+      <Main/>
       <InstagramCarousel />
       <Footer />
     </div>
